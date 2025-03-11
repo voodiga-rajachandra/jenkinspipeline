10fcef6017985fd6621903748ab0d175feb90326
Voodiga Rajachandra <vrcdevops@gmail.com>
2025-03-11 11:01:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,5 +18,5 @@ def calculate():
     return jsonify({"result": operations.get(operation, "Invalid operation")})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
